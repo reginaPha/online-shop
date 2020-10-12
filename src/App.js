47b0cb07d06ac4af771305ecc5e88bde0194f6fe
YOUR NAME <mandana-s@hotmail.de>
2020-10-12 12:21:17 +0200

    broken homepage into smaler components and finished styling menu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div >
-    <h1>Online Shop</h1>
+      <h1>Online Shop</h1>
       <HomePage />
     </div>
   );
