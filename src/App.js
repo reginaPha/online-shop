db493b353c3c442c58a4e79f5ddcd87671271e43
YOUR NAME <mandana-s@hotmail.de>
2020-10-13 14:19:33 +0200

    created header and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -8,10 +9,10 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div >
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop'  component={ShopPage}/>
-
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
